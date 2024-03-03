51bdd1480f957bbb020d3f83f45a080d56a4ea9d
LuizFelipeKraus <luiz.k1999@aluno.ifsc.edu.br>
2024-03-03 17:00:49 -0300

    first commit

---
+++ auth/src/main/java/com/store/auth/domain/User.java
@@ -0,0 +1,52 @@
+package com.store.auth.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="`user`")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional=true)
+    @JoinColumn(name = "company_id")
+    @JsonBackReference
+    private Company company;
+
+}
---
+++ auth/src/main/resources/data_2_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO "user"(name, username, email, password, role, company_id) VALUES ('Luiz Felipe Kraus','lk1999','lufekraus12@gmail.com','12345678', 1, 1);
+INSERT INTO "user"(name, username, email, password, role, company_id) VALUES ('Luiz Felipe Kraus Vendedor','lk2000','lufekraus@gmail.com','12345678', 1, Null);
